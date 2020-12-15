40f27a8f4a59e0516af13b4977f2d6605cb48094
Jerry Jia <jerry.jia@sap.com>
2020-12-15 21:27:22 +0800

    Revert "Forbid network cidr 100.64.0.0/10 (#170)"
    This reverts commit 1a57dfea87bcbccebb460d48567847c274ed6ccf.

--- pkg/validator/shoot_validator.go
+++ pkg/validator/shoot_validator.go
@@ -26,6 +26,13 @@ import (
 )
 
 func (v *Shoot) validateShoot(ctx context.Context, shoot *core.Shoot, infraConfig *alicloud.InfrastructureConfig) error {
+	// Network validation
+	networkPath := field.NewPath("spec", "networking")
+
+	if errList := alicloudvalidation.ValidateNetworking(shoot.Spec.Networking, networkPath); len(errList) != 0 {
+		return errList.ToAggregate()
+	}
+
 	// Provider validation
 	fldPath := field.NewPath("spec", "provider")
 
@@ -50,10 +57,8 @@ func (v *Shoot) validateShoot(ctx context.Context, shoot *core.Shoot, infraConfi
 
 func (v *Shoot) validateShootUpdate(ctx context.Context, oldShoot, shoot *core.Shoot) error {
 	var (
-		fldPath            = field.NewPath("spec")
-		networkingFldPath  = fldPath.Child("networking")
-		providerFldPath    = fldPath.Child("provider")
-		infraConfigFldPath = providerFldPath.Child("infrastructureConfig")
+		fldPath            = field.NewPath("spec", "provider")
+		infraConfigFldPath = fldPath.Child("infrastructureConfig")
 	)
 
 	// InfrastructureConfig update
@@ -73,11 +78,7 @@ func (v *Shoot) validateShootUpdate(ctx context.Context, oldShoot, shoot *core.S
 		}
 	}
 
-	if errList := alicloudvalidation.ValidateWorkersUpdate(oldShoot.Spec.Provider.Workers, shoot.Spec.Provider.Workers, providerFldPath.Child("workers")); len(errList) != 0 {
-		return errList.ToAggregate()
-	}
-
-	if errList := alicloudvalidation.ValidateNetworkingUpdate(oldShoot.Spec.Networking, shoot.Spec.Networking, networkingFldPath); len(errList) != 0 {
+	if errList := alicloudvalidation.ValidateWorkersUpdate(oldShoot.Spec.Provider.Workers, shoot.Spec.Provider.Workers, fldPath.Child("workers")); len(errList) != 0 {
 		return errList.ToAggregate()
 	}
 
@@ -85,13 +86,6 @@ func (v *Shoot) validateShootUpdate(ctx context.Context, oldShoot, shoot *core.S
 }
 
 func (v *Shoot) validateShootCreation(ctx context.Context, shoot *core.Shoot) error {
-	// Network validation
-	networkPath := field.NewPath("spec", "networking")
-	if errList := alicloudvalidation.ValidateNetworking(shoot.Spec.Networking, networkPath); len(errList) != 0 {
-		return errList.ToAggregate()
-	}
-
-	// Infra validation
 	fldPath := field.NewPath("spec", "provider")
 	infraConfig, err := checkAndDecodeInfrastructureConfig(v.decoder, shoot.Spec.Provider.InfrastructureConfig, fldPath.Child("infrastructureConfig"))
 	if err != nil {
