3f8659646d3a26cde0277dc4c2e78df18eed5316
ialidzhikov <i.alidjikov@gmail.com>
2021-03-29 13:24:55 +0300

    Clean up the deletion of legacy monitoring ConfigMap
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -264,11 +264,6 @@ func (vp *valuesProvider) GetControlPlaneChartValues(
 		}
 	}
 
-	// TODO: Remove this code in next version. Delete old config
-	if err := vp.deleteCCMMonitoringConfig(ctx, cp.Namespace); err != nil {
-		return nil, err
-	}
-
 	// Get control plane chart values
 	return vp.getControlPlaneChartValues(ctx, cpConfig, cp, cluster, checksums, scaledDown)
 }
