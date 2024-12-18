234c094bc603e4eff081f163abd88090fb19f4b9
Luca Bernstein <luca.bernstein@sap.com>
2024-12-18 03:25:05 +0100

    Remove the provider type predicates in webhooks (#749)

--- pkg/admission/mutator/webhook.go
+++ pkg/admission/mutator/webhook.go
@@ -5,13 +5,11 @@
 package mutator
 
 import (
-	extensionspredicate "github.com/gardener/gardener/extensions/pkg/predicate"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	corev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/gardener/gardener-extension-provider-alicloud/pkg/alicloud"
 )
@@ -21,10 +19,9 @@ var logger = log.Log.WithName("alicloud-mutator-webhook")
 // NewShootsWebhook creates a new mutation webhook for shoots.
 func NewShootsWebhook(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
 	return extensionswebhook.New(mgr, extensionswebhook.Args{
-		Provider:   alicloud.Type,
-		Name:       ShootMutatorName,
-		Path:       MutatorPath + "/shoots",
-		Predicates: []predicate.Predicate{extensionspredicate.GardenCoreProviderType(alicloud.Type)},
+		Provider: alicloud.Type,
+		Name:     ShootMutatorName,
+		Path:     MutatorPath + "/shoots",
 		Mutators: map[extensionswebhook.Mutator][]extensionswebhook.Type{
 			NewShootMutator(mgr): {{Obj: &corev1beta1.Shoot{}}},
 		},
