59dcd4918caa119a84e1dc97a95fbd873f89bea9
USER_TEST <kevin.zhang10@sap.com>
2025-07-23 15:57:22 +0800

    change WaitUntilExtensionObjectReady time

--- test/integration/bastion/bastion_test.go
+++ test/integration/bastion/bastion_test.go
@@ -235,13 +235,13 @@ var _ = Describe("Bastion tests", func() {
 			log,
 			bastion,
 			extensionsv1alpha1.BastionResource,
-			60*time.Second,
-			60*time.Second,
+			10*time.Second,
+			120*time.Second,
 			10*time.Minute,
 			nil,
 		)).To(Succeed())
 
-		time.Sleep(60 * time.Second)
+		time.Sleep(120 * time.Second)
 		verifyPort22IsOpen(ctx, c, bastion)
 		verifyPort42IsClosed(ctx, c, bastion)
 
