656799cf40b4a5f0d5d125ef501313a5b2a40149
Ishan Tyagi <ishan.tyagi@sap.com>
2025-08-06 11:01:50 +0530

    Add decoder to decode immutable config.

--- pkg/admission/validator/serialization.go
+++ pkg/admission/validator/serialization.go
@@ -9,11 +9,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
-	"github.com/gardener/gardener-extension-provider-alicloud/pkg/apis/alicloud"
+	apisali "github.com/gardener/gardener-extension-provider-alicloud/pkg/apis/alicloud"
 )
 
-func decodeControlPlaneConfig(decoder runtime.Decoder, cp *runtime.RawExtension, fldPath *field.Path) (*alicloud.ControlPlaneConfig, error) {
-	controlPlaneConfig := &alicloud.ControlPlaneConfig{}
+func decodeControlPlaneConfig(decoder runtime.Decoder, cp *runtime.RawExtension, fldPath *field.Path) (*apisali.ControlPlaneConfig, error) {
+	controlPlaneConfig := &apisali.ControlPlaneConfig{}
 	if err := util.Decode(decoder, cp.Raw, controlPlaneConfig); err != nil {
 		return nil, field.Invalid(fldPath, string(cp.Raw), "isn't a supported version")
 	}
@@ -21,8 +21,8 @@ func decodeControlPlaneConfig(decoder runtime.Decoder, cp *runtime.RawExtension,
 	return controlPlaneConfig, nil
 }
 
-func decodeInfrastructureConfig(decoder runtime.Decoder, infra *runtime.RawExtension, fldPath *field.Path) (*alicloud.InfrastructureConfig, error) {
-	infraConfig := &alicloud.InfrastructureConfig{}
+func decodeInfrastructureConfig(decoder runtime.Decoder, infra *runtime.RawExtension, fldPath *field.Path) (*apisali.InfrastructureConfig, error) {
+	infraConfig := &apisali.InfrastructureConfig{}
 	if err := util.Decode(decoder, infra.Raw, infraConfig); err != nil {
 		return nil, field.Invalid(fldPath, string(infra.Raw), "isn't a supported version")
 	}
@@ -30,7 +30,7 @@ func decodeInfrastructureConfig(decoder runtime.Decoder, infra *runtime.RawExten
 	return infraConfig, nil
 }
 
-func checkAndDecodeInfrastructureConfig(decoder runtime.Decoder, config *runtime.RawExtension, fldPath *field.Path) (*alicloud.InfrastructureConfig, error) {
+func checkAndDecodeInfrastructureConfig(decoder runtime.Decoder, config *runtime.RawExtension, fldPath *field.Path) (*apisali.InfrastructureConfig, error) {
 	if config == nil {
 		return nil, field.Required(fldPath, "InfrastructureConfig must be set for Alicloud shoots")
 	}
@@ -42,10 +42,23 @@ func checkAndDecodeInfrastructureConfig(decoder runtime.Decoder, config *runtime
 	return infraConfig, nil
 }
 
-func decodeCloudProfileConfig(decoder runtime.Decoder, config *runtime.RawExtension) (*alicloud.CloudProfileConfig, error) {
-	cloudProfileConfig := &alicloud.CloudProfileConfig{}
+func decodeCloudProfileConfig(decoder runtime.Decoder, config *runtime.RawExtension) (*apisali.CloudProfileConfig, error) {
+	cloudProfileConfig := &apisali.CloudProfileConfig{}
 	if err := util.Decode(decoder, config.Raw, cloudProfileConfig); err != nil {
 		return nil, err
 	}
 	return cloudProfileConfig, nil
+}
+
+// DecodeBackupBucketConfig decodes the `BackupBucketConfig` from the given `RawExtension`.
+func DecodeBackupBucketConfig(decoder runtime.Decoder, config *runtime.RawExtension) (*apisali.BackupBucketConfig, error) {
+	backupbucketConfig := &apisali.BackupBucketConfig{}
+
+	if config != nil && config.Raw != nil {
+		if err := util.Decode(decoder, config.Raw, backupbucketConfig); err != nil {
+			return nil, err
+		}
+	}
+
+	return backupbucketConfig, nil
 }
