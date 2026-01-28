35fc56be69538e78b22eafd0c135fe774864e921
USER_TEST <kevin.zhang10@sap.com>
2026-01-28 15:47:36 +0800

    update worker

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -90,7 +90,12 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 			return err
 		}
 
-		machineImage, err := w.findMachineImage(pool, infrastructureStatus, w.worker.Spec.Region)
+		machineTypeFromCloudProfile := gardencorev1beta1helper.FindMachineTypeByName(w.cluster.CloudProfile.Spec.MachineTypes, pool.MachineType)
+		if machineTypeFromCloudProfile == nil {
+			return fmt.Errorf("machine type %q not found in cloud profile %q", pool.MachineType, w.cluster.CloudProfile.Name)
+		}
+
+		machineImage, err := w.findMachineImage(pool, infrastructureStatus, w.worker.Spec.Region, machineTypeFromCloudProfile.Capabilities)
 		if err != nil {
 			return err
 		}
